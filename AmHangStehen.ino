07cc1ca68a66054e764e8430376ecdc2717611e5
husuch <50693353+husuch@users.noreply.github.com>
2019-09-25 14:15:35 +0200

    Update AmHangStehen.ino

--- AmHangStehen.ino
+++ AmHangStehen.ino
@@ -1,10 +1,3 @@
-/* This example drives each motor on the Zumo forward, then
-backward.  The yellow user LED is on when a motor should be
-running forward and off when a motor should be running backward.
-If a motor on your Zumo has been flipped, you can correct its
-direction by uncommenting the call to flipLeftMotor() or
-flipRightMotor() in the setup() function. */
-
 #include <Wire.h>
 #include <Zumo32U4.h>
 
